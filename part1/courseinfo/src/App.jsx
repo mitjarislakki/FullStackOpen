ec7399d3cf3d88b7f26df29c7517529357ede58c
Mitja <57709659+mitjarislakki@users.noreply.github.com>
2024-02-25 15:09:34 +0200

    Finish 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,36 @@
 function Header(props) {
-  return (<>
+  return (
+  <div>
   <h1>{props.course}</h1>
-  </>)
+  </div>)
+}
+
+function Part(props) {
+  return (
+  <div>
+  <p>{props.part} {props.exercises}</p>
+  </div>
+  )
 }
 
 function Content(props) {
   return (
-    <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-    </>
+    <div>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
   )
 }
 
 function Total(props) {
   return (
-    <>
+    <div>
     <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
-    </>
+    </div>
   )
 }
+
 
 function App() {
   const course = 'Half Stack application development'
