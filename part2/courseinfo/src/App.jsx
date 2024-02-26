16cdb524d76cbbadfb3bca0beec58a535182b540
Mitja <57709659+mitjarislakki@users.noreply.github.com>
2024-02-27 00:56:13 +0200

    Finish 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,27 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => <Part key={part.name} part={part}/>)}  
-  </>
-
-const Course = ({ course }) => {
-  return(
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((a, b) => a + b.exercises, 0)
-    }
-       />
-  </div>)
-}
+import Course from './components/Course'
 
 
 const App = () => {
